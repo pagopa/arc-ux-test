f612eee1baf5b932da7e03fbf98b6859373a9f12
Michele Moio <michelemoiodev@gmail.com>
2024-09-16 16:57:46 +0200

    e2e: [P4PU-451] login test

---
+++ tests/login.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Autenticazione e utente:', () => {
+
+	test("Come Cittadino voglio autenticarmi nell' ‘Area Riservata Cittadino’ per poter usufruire dei servizi offerti", async ({ page }) => {
+		const username = process.env?.USERNAME;
+		const password = process.env?.PASSWORD;
+		expect(username).toBeTruthy();
+		expect(password).toBeTruthy();
+
+		if(username && password) {
+			await page.goto('/');
+			await page.goto('/pagamenti/login');
+			await page.getByRole('button', { name: 'Accetta tutti' }).click();
+			await page.getByLabel('Accedi').click();
+			await page.getByRole('button', { name: 'Entra con SPID' }).click();
+			await page.getByLabel('demo', { exact: true }).click();
+			await page.waitForURL('**/start');
+			await expect(page.locator('#username')).toBeVisible({ timeout: 20000 });
+			await page.locator('#username').click();
+			await page.locator('#username').fill(username);
+			await page.locator('#username').press('Tab');
+			await page.getByLabel('Password').fill(password);
+			await page.getByRole('button', { name: 'Entra con SPID' }).click();
+			await page.getByRole('button', { name: 'Conferma' }).click();
+			await expect(page.getByLabel('app.dashboard.greeting')).toBeVisible();
+			const acessToken = await page.evaluate(() => localStorage.getItem('accessToken'));
+			expect(acessToken).toBeTruthy();
+		}
+	});
+
+})
