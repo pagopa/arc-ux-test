bc96dbb732ff1f8fcc4d671661aab0d68432568f
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2024-09-20 09:13:57 +0200

    feat: [P4PU-451] conditional auth phase, auth as setup (#2)

---
+++ utils.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+
+/** 'LOCAL' | 'DEV' | 'UAT' */
+export type ENVIRONMENT = 'LOCAL' | 'DEV' | 'UAT';
+export const ENVIRONMENTDEFAULT = 'LOCAL'
+export const userPath = 'auth/user.json';
+
+const ENVIRONMENT: ENVIRONMENT = process.env.ENVIRONMENT as ENVIRONMENT || ENVIRONMENTDEFAULT;
+
+// Auth phase can be skipped when the ENVIRONMENT === 'LOCAL' and a user.json file exists
+// please delete the user.json file when the user's info are outdated (token expired)
+export const skipAuth = (): boolean => ENVIRONMENT === 'LOCAL' && fs.existsSync(userPath);
