51d4978f6bef4ac75e0437a50cf79762baebad15
Vito Falco <vitofalco@gmail.com>
2024-09-26 14:37:32 +0200

    add logout test

---
+++ tests/logout.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test(`[E2E-ARC-3] Come Cittadino voglio 'sloggarmi' dall'applicativo`, async ({ page }) => {
+
+  await page.route('*/**/arc/v1/logout', async route => {
+    const json = {};
+    await route.fulfill({ json });
+  });
+
+  await page.goto('/pagamenti/');
+  await expect(page).toHaveURL('/pagamenti/');
+  await expect(page.getByLabel('party-menu-button')).toBeVisible();
+  await page.getByLabel('party-menu-button').click();
+  await page.getByRole('menuitem').getByText('Esci').click();
+  await page.waitForURL('**/login');
+
+  const accessToken = await page.evaluate(() => localStorage.getItem('accessToken'));
+	expect(accessToken).toBeNull();
+
+});
