d624b7df9b0c1145dcb86fc7de985fb33e40ea80
anislucidi01 <anis.lucidi@pagopa.it>
2024-09-30 17:17:34 +0200

    P4PU-556: Created test for receipts list error

---
+++ tests/receipts-for-user-error.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test if the error page is shown correctly to the user when an error occurs', async ({ page }) => {
+  await page.route('*/**/arc/v1/transactions*', async route => {
+    await route.fulfill({
+      status: 500,  
+      contentType: 'application/json', 
+    });
+  });
+
+  await page.goto('/pagamenti/');
+  await expect(page).toHaveURL('/pagamenti/');
+  await expect(page.getByTestId('ErrorOutlineIcon')).toBeVisible({timeout:50000});
+
+});
