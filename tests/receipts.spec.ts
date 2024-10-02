094de97d48dcddefece5146b255d105a4d708366
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2024-10-02 15:06:10 +0200

    feat: [P4PU-495] receipt PDF, E2E test (#16)

--- tests/receipts.spec.ts
+++ tests/receipts.spec.ts
@@ -1,22 +1,33 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { getFulfilledResponse, isValidDate } from '../utils';
 
-test("[E2E-ARC-9] Come Cittadino voglio accedere alla pagina di dettaglio di una ricevuta in modo da poter consultare tutte le informazioni disponibili", async ({ page }) => {
-	await page.goto('/pagamenti/');
-  await expect(page).toHaveURL('/pagamenti/')
+test.describe.configure({ mode: 'serial' });
 
+let page: Page;
+
+test.beforeAll(async ({ browser }) => { page = await browser.newPage(); });
+test.afterAll(async () => { await page.close(); });
+
+let transactionId;
+
+test("[E2E-ARC-9] Come Cittadino voglio accedere alla pagina di dettaglio di una ricevuta in modo da poter consultare tutte le informazioni disponibili", async () => {
+  await page.goto('/pagamenti/');
+  await expect(page).toHaveURL('/pagamenti/');
+  
   // waiting for the API call
   const listResponseBody = await getFulfilledResponse(page, 'arc/v1/transactions');
 
   // saving info of the first item
   const listItem = listResponseBody.transactions[0];
 
+  transactionId = listItem.transactionId;
+
   // click on first row item
   page.locator("table[aria-label='Storico table'] > tbody > tr").nth(0).click();
-  await expect(page).toHaveURL(`/pagamenti/transactions/${listItem.transactionId}`);
+  await expect(page).toHaveURL(`/pagamenti/transactions/${transactionId}`);
 
   // waiting for the API call
-  const { infoTransaction: transaction, carts } = await getFulfilledResponse(page, `arc/v1/transactions/${listItem.transactionId}`);
+  const { infoTransaction: transaction, carts } = await getFulfilledResponse(page, `arc/v1/transactions/${transactionId}`);
 
   // DATA CHECKS
   expect(listItem.transactionId === transaction.transactionId).toBeTruthy();
@@ -44,5 +55,14 @@ test("[E2E-ARC-9] Come Cittadino voglio accedere alla pagina di dettaglio di una
   // transactionId
   const transactionIdSubstring = transaction.transactionId.substring(0,7)
   await expect(page.getByText(new RegExp(`${transactionIdSubstring}`))).toBeVisible();
-
+});
+  
+test("[E2E-ARC-10] Come Cittadino voglio poter visualizzare il PDF di un avviso per poterlo stampare (eventualmente)", async ({ browserName }) => {
+  // Unfortunately with chromium this assertion doesn't work. I think it's a kind of bug
+  // https://github.com/microsoft/playwright/issues/6342
+  test.skip(browserName === 'chromium');
+  const pagePromise = page.waitForEvent('popup');  
+  await page.getByTestId('receipt-download-btn').click();
+  const newPage = await pagePromise;
+  await expect(newPage).toHaveURL(/blob:http(s)?:\/\/(dev.|uat.)?cittadini.pagopa.it\/([a-z]|[0-9]|-)*/);
 });
