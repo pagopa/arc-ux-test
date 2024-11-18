153371fd992e91992c6d31f3bcddf221599c1599
vito80ba <vitofalco@gmail.com>
2024-11-18 17:24:12 +0100

    Refact assistance test

--- tests/assistance.spec.ts
+++ tests/assistance.spec.ts
@@ -3,17 +3,16 @@ import { test, expect } from '@playwright/test';
 test('[E2E-ARC-11] Come Cittadino autenticato voglio accedere alla sezione di Assistenza in modo da aprire una segnalazione', async ({
   page
 }) => {
-  const newTicketURL = 'https://arc.assistenza.pagopa.it/hc/it/requests/new';
+  const newTicketURL = 'https://pagamenti.assistenza.pagopa.it/hc/it-it/requests/new';
   await page.goto('/pagamenti/');
   await expect(page).toHaveURL('/pagamenti/');
   const pagePromise = page.waitForEvent('popup');
   await page.getByRole('button', { name: 'Assistenza' }).click();
   const newPage = await pagePromise;
-  await newPage.getByTestId('confirm-email').click();
-  await newPage.getByTestId('confirm-email').fill('testuser@testmail.it');
-  await newPage.getByTestId('confirm-email').press('Tab');
-  await newPage.getByTestId('assistance-confirm-email').fill('testuser@testmail.it');
-  await newPage.getByTestId('assistance-confirm-email').press('Tab');
+  await expect(newPage.getByTestId('confirm-email')).not.toHaveValue('', {timeout: 2000});
+  const userEmail = await newPage.getByTestId('confirm-email').inputValue();
+  await newPage.getByTestId('assistance-confirm-email').fill(userEmail);
+  await newPage.getByTestId('assistance-confirm-email').dispatchEvent('change');
   await newPage.getByTestId('assistance-confirm-button').click();
   await expect(newPage).toHaveURL(newTicketURL, { timeout: 10000 });
 });
