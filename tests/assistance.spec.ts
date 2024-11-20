b3deaa48bce7dcf50d456f40e1374a1c12ca6410
vito80ba <vitofalco@gmail.com>
2024-11-20 13:22:52 +0100

    lint fix

--- tests/assistance.spec.ts
+++ tests/assistance.spec.ts
@@ -9,7 +9,7 @@ test('[E2E-ARC-11] Come Cittadino autenticato voglio accedere alla sezione di As
   const pagePromise = page.waitForEvent('popup');
   await page.getByRole('button', { name: 'Assistenza' }).click();
   const newPage = await pagePromise;
-  await expect(newPage.getByTestId('confirm-email')).not.toHaveValue('', {timeout: 2000});
+  await expect(newPage.getByTestId('confirm-email')).not.toHaveValue('', { timeout: 2000 });
   const userEmail = await newPage.getByTestId('confirm-email').inputValue();
   await newPage.getByTestId('assistance-confirm-email').fill(userEmail);
   await newPage.getByTestId('assistance-confirm-email').dispatchEvent('change');
