41e7423b63b83ce51cb2484cf41d9ebeaf9ae511
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-01-22 18:47:43 +0100

    fix: updating urls

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -40,7 +40,7 @@ test("[E2E-ARC-3] Come Cittadino voglio 'sloggarmi' dall'applicativo", async ({ 
   await expect(page.getByLabel('party-menu-button')).toBeVisible();
   await page.getByLabel('party-menu-button').click();
   await page.getByRole('menuitem').getByText('Esci').click();
-  await page.waitForURL('**/login');
+  await page.waitForURL('**/accesso');
 
   const accessToken = await page.evaluate(() => localStorage.getItem('accessToken'));
   expect(accessToken).toBeNull();
