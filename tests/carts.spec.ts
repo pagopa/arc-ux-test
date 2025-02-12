bf3cd70912f831fbf6af406bc723ab3fa5189f99
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-02-12 16:50:01 +0100

    [P4PU-726]: implement utility function for amount formatting and update E2E tests to use it

--- tests/carts.spec.ts
+++ tests/carts.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, Page } from '@playwright/test';
+import { amountToEur } from '../utils/index';
 
 let page: Page;
 
@@ -10,88 +11,90 @@ test.beforeAll(async ({ browser }) => {
 
 test(`[E2E-ARC-12] Come Cittadino autenticato voglio poter inserire più avvisi di pagamento nel carrello in modo da poter effettuare un solo pagamento e risparmiare sui costi di commissione`, async () => {
   await page.goto(`/pagamenti/`);
-
+  // setting this item to avoid the optin modal
   await page.evaluate(() => {
     window.sessionStorage.setItem('OPTIN', 'true');
   });
-
   await page.goto(`/pagamenti/avvisi/`);
   await expect(page).toHaveURL(`/pagamenti/avvisi/`);
 
+  // waiting for the /payment-notices response
   const responsePromise = page.waitForResponse('**/arc/v1/payment-notices');
   const response = await responsePromise.then((response) => response.json());
 
   // wait for the list of payment notices
   await expect(page.locator('#payment-notices-list')).toBeVisible({ timeout: 10000 });
   const optionsListItemsCount = await page.getByTestId('payment-notices-item').count();
-  expect(optionsListItemsCount).toBeGreaterThan(0);
+  // this test should have at least 2 items
+  expect(optionsListItemsCount).toBeGreaterThan(1);
 
   // select the first item of the list showed
   const selectedItem = response['paymentNotices'][0];
   const listFirstItem = page.getByTestId('payment-notices-item').first();
+  const firstItemAmount = selectedItem.paymentOptions[0].amount;
+  // click on first item to go to the detail page
   await listFirstItem.getByTestId('payment-notices-item-cta').click();
-
   await expect(page).toHaveURL(`/pagamenti/avvisi/${selectedItem.iupd}/${selectedItem.paTaxCode}`);
 
   // toggle add/remove button
-  const cartButton = page.locator('#payment-notice-pay-button');
-  // cart amount element
-  const cartAmount = page.locator('#header-cart-amount');
+  const cartButton = page.locator('#payment-notice-add-button');
+  const headerCartAmount = page.locator('#header-cart-amount');
 
-  await expect(cartAmount).toHaveText('0,00 €');
+  await expect(headerCartAmount).toHaveText(amountToEur(0));
 
   // add element to cart
   await cartButton.click();
-
-  await expect(cartAmount).not.toHaveText('0,00 €');
+  await expect(headerCartAmount).toHaveText(amountToEur(firstItemAmount));
 
   // close the drawer
   await page.getByTestId('CloseIcon').click();
 
   // remove the item
   await cartButton.click();
-  await expect(cartAmount).toHaveText('0,00 €');
+  await expect(headerCartAmount).toHaveText(amountToEur(0));
 
   // add the item again to continue with the test
   await cartButton.click();
   // close the drawer again
   await page.getByTestId('CloseIcon').click();
 
-  // second step
+  // second item
   await page.goto(`/pagamenti/avvisi/`);
   await expect(page).toHaveURL(`/pagamenti/avvisi/`);
 
-  // wait for the list of payment notices
+  // wait for the list of payment notices again
   await expect(page.locator('#payment-notices-list')).toBeVisible({ timeout: 10000 });
-  //const optionsListItemsCount = await page.getByTestId('payment-notices-item').count();
-  //expect(optionsListItemsCount).toBeGreaterThan(0);
 
   // select the second item of the list showed
   const selectedSecondItem = response['paymentNotices'][1];
   const listSecondItem = page.getByTestId('payment-notices-item').nth(1);
+  const secondItemAmount = selectedSecondItem.paymentOptions[0].amount;
+  const totalAmountInEur = amountToEur(firstItemAmount + secondItemAmount);
   await listSecondItem.getByTestId('payment-notices-item-cta').click();
 
-  await expect(page).toHaveURL(`/pagamenti/avvisi/${selectedSecondItem.iupd}/${selectedSecondItem.paTaxCode}`);
+  await expect(page).toHaveURL(
+    `/pagamenti/avvisi/${selectedSecondItem.iupd}/${selectedSecondItem.paTaxCode}`
+  );
 
-  //add second element to cart
+  // add second element to cart
   await cartButton.click();
 
-  await expect(cartAmount).toHaveText('8,62 €');
+  await expect(headerCartAmount).toHaveText(totalAmountInEur);
 
-  // payment
+  // payment button click
   await page.locator('#pay-button').click();
 
   // wait for checkout
   await expect(page).toHaveURL(new RegExp('checkout.pagopa.it/'));
-  // test on checkout side
-  //await expect(page.locator('#email')).toBeVisible({ timeout: 10000 });
   // test if the car button as the same amount of the payment notice
-  await expect(page.getByRole('button').getByText('8,62 €')).toBeVisible({ timeout: 10000 });
+  await expect(page.getByRole('button').getByText(totalAmountInEur)).toBeVisible({
+    timeout: 10000
+  });
 });
 
 test(`[E2E-ARC-12B] Come Cittadino autenticato voglio poter inserire più avvisi di pagamento nel carrello in modo da poter effettuare un solo pagamento e risparmiare sui costi di commissione ma si verifica un errore`, async () => {
+  // 423(avvio-pagamento) error
   await page.goBack();
-
   page.route(
     '**/checkout/ec/v1/carts',
     async (route) => {
@@ -99,20 +102,13 @@ test(`[E2E-ARC-12B] Come Cittadino autenticato voglio poter inserire più avvisi
     },
     { times: 1 }
   );
-
-  // cart amount element
-  const cartAmount = page.locator('#drawer-cart-amount');
-  await expect(cartAmount).toHaveText('8,62 €');
-
-  // open the drawer
-  // await cartAmount.click();
-  // pay button click
+  const drawerCartAmount = page.locator('#drawer-cart-amount');
+  await expect(drawerCartAmount).not.toHaveText(amountToEur(0));
   await page.locator('#pay-button').click();
-
   await expect(page).toHaveURL('/pagamenti/errore/avvio-pagamento');
 
+  // 422(avviso-non-pagabile) error
   await page.goBack();
-
   page.route('**/checkout/ec/v1/carts', async (route) => {
     await route.fulfill({
       json: {
@@ -123,8 +119,6 @@ test(`[E2E-ARC-12B] Come Cittadino autenticato voglio poter inserire più avvisi
       status: 422
     });
   });
-
   await page.locator('#pay-button').click();
-
   await expect(page).toHaveURL('/pagamenti/errore/avviso-non-pagabile');
 });
